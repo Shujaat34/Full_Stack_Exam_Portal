266e523e624c6b45c72757e193644f90334cf804
Syed Shujaat Hussain <shujaatalibukhari786@gmail.com>
2022-01-04 19:16:11 +0500

    third-commit-radio-button-done

--- Full_Stack_Exam_Portal/front-end/examportal/src/app/pages/user/start-quiz/start-quiz.component.ts
+++ Full_Stack_Exam_Portal/front-end/examportal/src/app/pages/user/start-quiz/start-quiz.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Question } from 'src/app/question';
 import { QuestionService } from 'src/app/services/question.service';
-import {MatRadioModule} from '@angular/material/radio';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-start-quiz',
@@ -17,7 +17,14 @@ export class StartQuizComponent implements OnInit {
     private questionService : QuestionService) { }
   quizId : number = 0;
 
-  questions : Question[] = [];
+  questions : Question[]= [new Question()];
+
+  marksGot = 0;
+  correctAnswers = 0;
+  attempted = 0;
+  perQuestionMarks =0; 
+
+  isSubmit = false;
 
   ngOnInit(): void {
     this.preventBackButton();
@@ -29,10 +36,52 @@ export class StartQuizComponent implements OnInit {
       (resp : Question[])=>{
         this.questions = resp;
         console.log(this.questions);
+
+        this.questions.forEach((q)=>{
+          q.givenAnswer = '';
+        });
       },(error : HttpErrorResponse)=>{
         console.log('Error Loading Questions of Quiz '+error.message);
       }
     );
+  }
+
+  submitQuiz(){
+    Swal.fire({
+      title:'Do you want to Submit the Quiz',
+      confirmButtonText:'Yes',
+      showCancelButton:true,
+      icon:'info'
+    }).then((result)=>{
+      if(result.isConfirmed){
+        // console.log('Got These')
+        // console.log(this.questions);
+        // this.router.navigate(['/start-quiz/'+this.quiz.id]);
+
+        this.isSubmit = true;
+        
+        this.questions.forEach((q)=>{
+          if(q.givenAnswer == q.answer){
+            this.correctAnswers++;
+            this.perQuestionMarks = +this.questions[0].quiz.maxMarks/this.questions.length;
+          }
+          if(q.givenAnswer.trim() != ''){
+            this.attempted++;
+          }
+        });
+
+        //makrs after two decimal points.
+        this.marksGot = +(this.perQuestionMarks * this.correctAnswers).toFixed(2);
+
+        
+     
+
+        console.log('Got Marks '+this.marksGot);
+        console.log('Attempted '+this.attempted);
+        console.log('Correct Answers '+this.correctAnswers);
+
+      }
+    });
   }
 
   preventBackButton(){
